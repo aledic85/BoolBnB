ad732e597ad9ec4e263a50e733703d06b83d1701
aledic85 <dicuollo85@gmail.com>
2019-06-18 17:21:57 +0200

    add CRUD operation on apartment

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="wrapper">
   <div class="container dashB">
     <div class="boxAddNew">
-      <h2>Benvenuto nella tua dashboard {{ Auth::user() -> find(10) -> name }}</h2>
+      <h2>Benvenuto nella tua dashboard {{ Auth::user() -> name }}</h2>
       <form class="" action="{{ route('new.apart') }}" method="get">
         @csrf
         @method('POST')
@@ -14,23 +14,26 @@
     <div class="box-apartments">
       @foreach ($apartments as $apartment)
         <div class="box-apartment rounded-bottom">
-          <img src="{{ $apartment -> img_path }}">
+          <img src="{{ URL::to('/storage') }}/images/{{ $apartment->img_path }}">
           <div class="infoAp p-2">
             <h3>{{ $apartment -> title }}</h3>
             <p>{{ $apartment -> description }}</p>
-            <span>Via Giuseppe Garibaldi, 11A {{ $apartment -> latitude }} {{ $apartment -> longitude }}</span>
+            <p>Via Giuseppe Garibaldi, 11A {{ $apartment -> latitude }} {{ $apartment -> longitude }}</p>
             <ul>
               <li>Numero stanze: {{ $apartment -> rooms }}</li>
               <li>Numero letti: {{ $apartment -> beds }}</li>
               <li>Numero bagni: {{ $apartment -> bathrooms }}</li>
               <li>Metri quadrati: {{ $apartment -> mq }}</li>
             </ul>
-            <form class="ed" action="#" method="post">
-              @csrf
-              @method('PATCH')
-              <button type="submit" name="edit">MODIFICA</button>
-            </form>
-            <form class="del" action="#" method="post">
+            <div class="char-wrapper">
+              <span>Wi-Fi: {{$apartment->wi_fi}}, </span>
+              <span>Parking space: {{$apartment->parking_space}}, </span>
+              <span>Pool: {{$apartment->pool}}, </span>
+              <span>Sauna: {{$apartment->sauna}}, </span>
+              <span>Annuncio attivo: {{$apartment->active}}</span>
+            </div>
+            <a href="{{route('edit.apart', $apartment->id)}}"><button type="submit" name="edit">MODIFICA</button></a>
+            <form class="del" action="{{route('delete.apart', $apartment-> id)}}" method="post">
               @csrf
               @method('DELETE')
               <button type="submit" name="delete">ELIMINA</button>
@@ -41,5 +44,22 @@
     </div>
   </div>
 </div>
+
+<script type="text/javascript">
+
+  var wrap = $('.char-wrapper');
+  var text = wrap.text();
+  console.log(text);
+  wrap.each(function(i) {
+
+    if (this.text == 0) {
+
+      this.text = 'no';
+    } else if(this.text == 1){
+
+      this.text = 'sì';
+    }
+  });
+</script>
 
 @stop
