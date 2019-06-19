a650ef04534d68a8fa2cbcec17689ebf602d4c84
MarkoM91 <markomedic91@gmail.com>
2019-06-19 16:21:21 +0200

    mockup3/modifica1

---
+++ resources/views/page/show-apart.blade.php
@@ -0,0 +1,40 @@
+@extends('layout.home-layout')
+    @section('content')
+
+      <div class="details">
+          <div class="img-container">
+              <img src="http://3.citynews-milanotoday.stgy.ovh/~media/original-hi/68320916180522/milano-panoramica-4.jpg">
+          </div>
+          <div class="details-middle">
+              <div class="middle-left">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </div>
+              <div class="middle-right">
+                  <ul>
+                    <li>MMMM</li>
+                    <li>MMMM</li>
+                    <li>MMMM</li>
+                    <li>MMMM</li>
+                    <li>MMMM</li>
+                    <li>MMMM</li>
+                    <li>MMMM</li>
+                  </ul>
+              </div>
+          </div>
+          <div class="details-down">
+              <div class="down-left">
+                  <div class="map-container">
+                     <div class="map">
+
+                     </div>
+                  </div>
+              </div>
+              <div class="down-right">
+                  <h1>Scrivi al Proprietario</h1>
+                  <input id="email" type="text" name="" placeholder="Email">
+                  <input id="message" type="text" name="" placeholder="Messaggio">
+              </div>
+          </div>
+      </div>
+
+@stop
