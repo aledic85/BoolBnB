125e75f695b3e647d9be80cdf6a47d8d21150ab2
aledic85 <dicuollo85@gmail.com>
2019-07-01 11:04:31 +0200

    fix bug in apt-chart

--- resources/views/page/show-stats.blade.php
+++ resources/views/page/show-stats.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.home-layout')
 
 @section('content')
+
  <h1>Numero totale visualizzazioni appartamento = {{$totalViews}}</h1>
  <h1>Numero totale messaggi ricevuti appartamento = {{$totalMessages}}</h1>
  <div class="chart-cont">
@@ -10,13 +11,22 @@
  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
  <script type="text/javascript">
 
-  var views = {{$totalViews}};
   var months = {!! json_encode($months) !!};
 
-  var uniqueMonths = [];
-    $.each(months, function(i, el){
-      if($.inArray(el, uniqueMonths) === -1) uniqueMonths.push(el);
-    });
+  var month = [], views = [], prev;
+
+  for ( var i = 0; i < months.length; i++ ) {
+      if ( months[i] !== prev ) {
+          month.push(months[i]);
+          views.push(1);
+      } else {
+          views[views.length-1]++;
+      }
+      prev = months[i];
+  }
+
+  console.log(views);
+
 
  var ctx = document.getElementById('myChart').getContext('2d');
  var chart = new Chart(ctx, {
@@ -25,12 +35,12 @@
 
    // The data for our dataset
    data: {
-       labels: uniqueMonths,
+       labels: month,
        datasets: [{
            label: 'Visualizzazioni per mese',
-           backgroundColor: 'rgb(255, 99, 132)',
+           backgroundColor: ['red', 'blue'],
            borderColor: 'rgb(255, 99, 132)',
-           data: [views]
+           data: views
        }]
    },
 
